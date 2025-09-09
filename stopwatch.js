75e75444a2c192c5fff599faf6c3a42c58e7591d
Ali İslam <bgane8181@gmail.com>
2025-09-09 22:33:49 +0300

    İlk yükleme - Stopwatch

---
+++ stopwatch.js
@@ -0,0 +1,41 @@
+let second=0;
+let tens=0;
+let interval;
+
+getSeconds=document.querySelector(".second");
+getTens=document.querySelector(".tens");
+
+let btnStart=document.querySelector(".btnStart");
+let btnStop=document.querySelector(".btnStop");
+let btnReset=document.querySelector(".btnReset");
+
+btnStart.addEventListener("click",() => {
+interval=setInterval(timer,10);
+});
+btnStop.addEventListener("click", () => {clearInterval(interval)
+});
+btnReset.addEventListener("click", () =>{
+tens="00";
+second="00";
+getSeconds.innerHTML=second;
+getTens.innerHTML=tens;
+});
+
+function timer(){
+    tens++;
+if (tens <=9) {
+    getTens.innerHTML = "0" + tens;
+}
+if (tens > 9) {
+    getTens.innerHTML = tens;
+}
+if (tens>99){
+   second++;
+   getSeconds.innerHTML="0"+second;
+   tens = 0;
+   getTens.innerHTML = "0" + tens;
+}
+if(second > 9) {
+getSeconds.innerHTML = second;
+}}
+
